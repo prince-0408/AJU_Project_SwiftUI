efd55f08c5623069fae06c5d50ecc2864be7576c
prince-0408 <princekuyadav5646@gmail.comgit config --global user.name prince-0408>
2021-09-26 18:43:42 +0530

    Initial Commit

---
+++ AJU_Project/AJU_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AJU_ProjectApp.swift
+//  AJU_Project
+//
+//  Created by Anand Yadav on 26/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AJU_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AJU_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AJU_Project
+//
+//  Created by Anand Yadav on 26/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
