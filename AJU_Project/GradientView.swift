38e7d1cad9c6906e3aab4ea676426bc5d411e805
Prince Yadav <princekuyadav5646@gmail.com>
2022-03-02 21:01:51 +0530

    UI

--- AJU_Project/GradientView.swift
+++ AJU_Project/GradientView.swift
@@ -6,7 +6,7 @@ struct GradientView: View{
         LinearGradient(gradient: Gradient(colors:[
                                             Color(#colorLiteral(red: 0.14647156, green: 0.5010695457, blue: 0.4824878573, alpha: 1)),
                                             Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
-                                            Color(#colorLiteral(red: 0.1961190403, green: 0.3681041598, blue: 0.3569169641, alpha: 1))
+                                            Color(#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1))
         ]
         ),
         startPoint: .top, endPoint: .bottom)
